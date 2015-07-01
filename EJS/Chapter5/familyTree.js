537461f8fa6b54a4c7a71cbab5a5754a4da818e0
Chrinkus <seeschickrun@gmail.com>
2015-06-30 23:46:57 -0500

    Added notes to familyTree.js

--- EJS/Chapter5/familyTree.js
+++ EJS/Chapter5/familyTree.js
@@ -1,4 +1,18 @@
 var ANCESTRY = require('./ancestry');
 
 var ancestry = JSON.parse(ANCESTRY);
-console.log(ancestry.length);
+// console.log(ancestry.length);
+
+function filter(array, test) { // filter an array using passed test function
+    var passed = [];
+    for (var i = 0; i < array.length; i++) {
+        if (test(array[i])) { // if test passes push elements into new array
+            passed.push(array[i]);
+        }
+    }
+    return passed;
+}
+
+console.log(filter(ancestry, function(person) { // unnamed callback as test function
+    return person.born > 1900 && person.born < 1925;
+}));

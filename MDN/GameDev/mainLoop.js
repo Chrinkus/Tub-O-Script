853dc5c9b8c32047780258000365fde37f1a3262
Chrinkus <seeschickrun@gmail.com>
2016-01-26 15:00:06 -0600

    Animation work. main loops

--- MDN/GameDev/mainLoop.js
+++ MDN/GameDev/mainLoop.js
@@ -7,13 +7,40 @@ window.main = function() {
 
 main(); // Start the cycle*/
 
-// A better main loop
+/* A better main loop
+// Assumes MyGame was previously defined
 (function () {
     function main() {
-        window.requestAnimationFrame(main);
+        MyGame.stopMain = window.requestAnimationFrame(main);
 
         // Whatever main loop needs to do
+        // At any point we can stop the main loop using our stopMain token with:
+        // window.cancelAnimationFrame(MyGame.stopMain);
     }
 
     main();
+})();*/
+
+/* A more optimized main loop
+(function() {
+    function main(tFrame) {
+        MyGame.stopMain = window.requestAnimationFrame(main);
+
+        // Whatever main loop needs to do
+        // tFrame is now a DOMHighResTimeStamp provided by rAF
+    }
+
+    main(); // start the cycle
+})();*/
+
+// What most browser games should look like
+(function() {
+    function main(tFrame) {
+        MyGame.stopMain = window.requestAnimationFrame(main);
+
+        update(tFrame); // Call your update method. We pass it tFrame
+        render();
+    }
+
+    main(); // Start cycle
 })();
